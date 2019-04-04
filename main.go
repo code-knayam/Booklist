d5e61af89a41fc191e196f735955815583fc5a5b
code-knayam <mayankjain1394@gmail.com>
2019-04-04 23:08:01 +0530

    Added env file.

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
+
+	"github.com/lib/pq"
+
+	"github.com/subosito/gotenv"
 
 	"github.com/gorilla/mux"
 )
@@ -18,13 +21,22 @@ type Book struct {
 
 var books []Book
 
+func init() {
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
-	books = append(books, Book{ID: 1, Title: "Book 1", Author: "Author 1", Year: "2001"},
-		Book{ID: 2, Title: "Book 2", Author: "Author 2", Year: "2001"},
-		Book{ID: 3, Title: "Book 3", Author: "Author 3", Year: "2001"},
-		Book{ID: 4, Title: "Book 4", Author: "Author 4", Year: "2001"},
-		Book{ID: 5, Title: "Book 5", Author: "Author 5", Year: "2001"})
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+	logFatal(err)
+
+	log.Println(pgURL)
 
 	router := mux.NewRouter()
 
@@ -39,63 +51,20 @@ func main() {
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get books is called")
-
-	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get book is called")
-
-	params := mux.Vars(r)
-
-	// type conversion to int from string
-	id, _ := strconv.Atoi(params["id"])
-
-	for _, book := range books {
-		if book.ID == id {
-			json.NewEncoder(w).Encode(&book)
-		}
-	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Add book is called")
-
-	var book Book
-	json.NewDecoder(r.Body).Decode(&book)
-
-	books = append(books, book)
-
-	json.NewEncoder(w).Encode(books)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Update book is called")
-
-	var book Book
-	json.NewDecoder(r.Body).Decode(&book)
-
-	for i, item := range books {
-		if item.ID == book.ID {
-			books[i] = book
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
 	log.Println("Remove book is called")
-
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-
-	for i, item := range books {
-		if item.ID == id {
-			books = append(books[:i], books[i+1:]...)
-		}
-	}
-
-	json.NewEncoder(w).Encode(books)
-
 }
